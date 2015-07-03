11ce81eeae0885b9ec6ba8b363e3062fba4ab29c
Tim Beiko <t.beiko23@gmail.com>
2015-07-03 10:29:17 -0400

    Rewrite tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,14 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new
-    video.title = "Family Guy"
-    video.description = "Adventures of Peter's Family"
-    video.small_cover_url = "/tmp/family_guy.jpg"
-    video.large_cover_url = "/tmp/monk_large.jpg"
-    video.category_id = 1
-    video.save
-    Video.first.title.should == "Family Guy"
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
