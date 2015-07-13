bbbf935d548e27add121b841e1f993f6fd956876
Tim Beiko <t.beiko23@gmail.com>
2015-07-13 09:39:02 -0400

    Fix styling and forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,18 @@ class SessionsController < ApplicationController
     user = User.where(email: params[:email]).first
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to home_path, notice: "You are signed in, enjoy!"
+      flash[:success] = "You are signed in, enjoy!"
+      redirect_to home_path
     else
-      flash[:error] = "Invalid email or password."
-      redirect_to sign_in
+      flash[:danger] = "Invalid email or password."
+      redirect_to sign_in_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "You are signed out"
+    flash[:success] = "You are signed out"
+    redirect_to root_path
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to sign_in_path
     else
       render :new
     end
