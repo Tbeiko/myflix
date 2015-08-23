fae1729a5d4a2bd37a21d84555aff65ce1a32caa
Tim Beiko <t.beiko23@gmail.com>
2015-08-23 12:54:21 -0400

    Refactor tests

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -20,37 +20,35 @@ describe RelationshipsController do
     let(:action) { get :index }
   end
 
-  describe "DELETE destroy" do 
+  describe "DELETE destroy" do
+    let(:user1) { Fabricate(:user) } 
+    let(:user2) { Fabricate(:user) } 
+
+    before do 
+      set_current_user(user1)
+    end
+
     it_behaves_like "require signed in user" do 
       let(:action) { delete :destroy, id: 4 }
     end
 
     it "deletes the relationship if the current user is the follower" do 
-      user1 = Fabricate(:user)
-      set_current_user(user1)
-      user2 = Fabricate(:user)
       relationship = Fabricate(:relationship, follower: user1, leader: user2)
       delete :destroy, id: relationship
       expect(Relationship.count).to eq(0)
     end
 
     it "redirects to the people page" do 
-      user1 = Fabricate(:user)
-      set_current_user(user1)
-      user2 = Fabricate(:user)
       relationship = Fabricate(:relationship, follower: user1, leader: user2)
       delete :destroy, id: relationship
       expect(response).to redirect_to people_path
     end
+
     it "does not delete the relationship if the current user is not the follower" do 
-      user1 = Fabricate(:user)
-      set_current_user(user1)
-      user2 = Fabricate(:user)
       user3 = Fabricate(:user)
       relationship = Fabricate(:relationship, follower: user3, leader: user2)
       delete :destroy, id: relationship
       expect(Relationship.count).to eq(1)
     end
-
   end
 end 
