63f0bb17e978c968aaccc4044129e6e9c0ef1f8a
Tim Beiko <t.beiko23@gmail.com>
2015-08-28 15:12:17 -0400

    Refactor tests

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe PasswordResetsController do 
   describe "GET show" do 
+    let(:user) { Fabricate(:user) }
+
     it "renders show template if the token is valid" do 
-      user = Fabricate(:user)
       get :show, id: user.token
       expect(response).to render_template :show
     end
 
     it "sets @token" do 
-      user = Fabricate(:user)
       get :show, id: user.token
       expect(assigns(:token)).to eq(user.token)
     end
@@ -22,26 +22,24 @@ describe PasswordResetsController do
 
   describe "POST create" do 
     context "with valid token" do 
+      let(:user) { Fabricate(:user, password: "oldpassword") }
+
       it "redirects to the sign in page" do 
-        user = Fabricate(:user, password: "oldpassword")
         post :create, token: user.token, password: "newpassword"
         expect(response).to redirect_to sign_in_path
       end
 
       it "updates the user's password" do 
-        user = Fabricate(:user, password: "oldpassword")
         post :create, token: user.token, password: "newpassword"
         expect(user.reload.authenticate('newpassword')).to be_truthy 
       end
 
       it "sets the flash success message" do 
-        user = Fabricate(:user, password: "oldpassword")
         post :create, token: user.token, password: "newpassword"
         expect(flash[:success]).to be_present
       end
 
       it "regenerates the users's token" do 
-        user = Fabricate(:user, password: "oldpassword")
         old_token = user.token
         post :create, token: user.token, password: "newpassword"
         expect(user.reload.token).not_to eq(old_token)
