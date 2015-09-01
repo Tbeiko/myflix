de59d3e0dedb43107488987eb17a557ed4dff95b
Tim Beiko <t.beiko23@gmail.com>
2015-09-01 11:20:00 -0400

    Add confirmation of password when updating

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,19 +10,23 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.find_by(token: params[:token])
-    if user && user.update(password: params[:password])
-      flash[:success] = "Your password was successfully updated."
-      redirect_to sign_in_path
-    elsif user
-      @token = user.token
-      flash[:danger] = "Invalid password."
+    if params[:password] == params[:confirm_password]
+      if user && user.update(password: params[:password])
+        flash[:success] = "Your password was successfully updated."
+        redirect_to sign_in_path
+      elsif user
+        @token = user.token
+        flash[:danger] = "Invalid password."
+        render :show
+      else
+        redirect_to expired_token_path
+      end
+      if user
+        user.remove_token
+      end
+    else 
+      flash[:danger] = "The passwords didn't match"
       render :show
-    else
-      redirect_to expired_token_path
-    end
-
-    if user
-      user.remove_token
     end
   end
 end
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -35,31 +35,50 @@ describe PasswordResetsController do
       end
 
       it "redirects to the sign in page" do 
-        post :create, token: user.token, password: "newpassword"
+        post :create, token: user.token, password: "newpassword", confirm_password: "newpassword"
         expect(response).to redirect_to sign_in_path
       end
 
       it "updates the user's password" do 
-        post :create, token: user.token, password: "newpassword"
+        post :create, token: user.token, password: "newpassword", confirm_password: "newpassword"
         expect(user.reload.authenticate('newpassword')).to be_truthy 
       end
 
       it "sets the flash success message" do 
-        post :create, token: user.token, password: "newpassword"
+        post :create, token: user.token, password: "newpassword", confirm_password: "newpassword"
         expect(flash[:success]).to be_present
       end
 
       it "removes the users's token" do 
         old_token = user.token
-        post :create, token: user.token, password: "newpassword"
+        post :create, token: user.token, password: "newpassword", confirm_password: "newpassword"
         expect(user.reload.token).to be_nil
       end
     end
 
     context "with invalid token" do 
       it "redirects to the expired token path" do 
-        post :create, token: "12345" , password: "newpassword"
+        post :create, token: "12345" , password: "newpassword", confirm_password: "newpassword"
         expect(response).to redirect_to expired_token_path
+      end
+    end
+
+    context "with non-matching passwords" do 
+      let(:user) { Fabricate(:user, password: "oldpassword") }
+
+      # This would happen when the user clicks the forgot password link
+      before do
+        user.generate_token
+      end
+
+      it "renders an error message" do 
+        post :create, token: user.token, password: "newpassword", confirm_password: "anotherpassword"
+        expect(flash[:danger]).to be_present
+      end
+
+      it "renders the show template" do 
+        post :create, token: user.token, password: "newpassword", confirm_password: "anotherpassword"
+        expect(response).to render_template :show
       end
     end
   end
