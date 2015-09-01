7a0115fd8f1f919612c2f6cb8ab14e2e2c809ff7
Tim Beiko <t.beiko23@gmail.com>
2015-09-01 10:29:21 -0400

    Change defaut from email

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,11 +1,13 @@
 class AppMailer < ActionMailer::Base
+  default from: "info@myflix.com"
+
   def send_welcome_email(user)
     @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Welcome to Myflix!"
+    mail to: user.email, subject: "Welcome to Myflix!"
   end
 
   def send_forgot_password(user)
     @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Password Reset Link"
+    mail to: user.email, subject: "Password Reset Link"
   end
 end
