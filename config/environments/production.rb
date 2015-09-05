e5dff0290e676f4be2df47c7734e005261213302
Tim Beiko <t.beiko23@gmail.com>
2015-09-05 17:08:03 -0400

    Change domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Myflix::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'https://agile-sierra-2595.herokuapp.com',
+    :domain         => 'https://timflix.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
