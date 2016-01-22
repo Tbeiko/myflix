bc964a17af138f3bb4202098a028c6d9d258388a
tbeiko <t.beiko23@gmail.com>
2016-01-22 10:37:06 -0500

    Change controllers and mailers to only pass ID

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@
     @user = User.new(user_params)
     if @user.save
       handle_invitation
-      AppMailer.send_welcome_email(@user).deliver
+      AppMailer.delay.send_welcome_email(@user.id)
       redirect_to sign_in_path
     else
       render :new
